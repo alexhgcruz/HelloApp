c49a0489817fe06a373c78cc8a9aa09679559099
Alex Cruz <alexh_ata@hotmail.com>
2024-01-11 21:46:46 -0300

    Primeiro Commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,18 @@
+﻿//Variaveis.
+
+string nome;
+int idade;
+
+Console.WriteLine("Olá, tudo bem?");
+
+Console.WriteLine("Qual o seu nome?");
+
+nome = Console.ReadLine();
+
+Console.WriteLine("Bem vindo " + nome + "!!");
+Console.WriteLine("É um prazer te conhecer!!");
+Console.WriteLine("Qual a sua idade?");
+
+idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Parabeéns!! Voce tem " + " anos!!")
